21695a9b86823d15552da185af0ce3138118116b
yyqq188 <83791248@qq.com>
2019-06-16 15:15:13 +0800

    all second commit

---
+++ dynamic_page.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
+class Dynimic_process():
+    enterprise_source_table = "dict_tmp_hz_qy_name"
+    enterprise_list = []
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument("--no-sandbox")
+        self.client = webdriver.Chrome(executable_path="./chromedriver",chrome_options=chrome_options)
+
+        dispatcher.connect(self.spider_closed,signals.spider_closed)
+
+
+
+    def spider_closed(self):
+        self.client.quit()
+
+    def start_requests(self,url):
+        self.client.get(url)
+        time.sleep(1)
+        return self.client.page_source
+
+
+if __name__ == "__main__":
+    Dynimic_process().start_requests(url="http://zfcg.wlmq.gov.cn/infopublish.do?method=infoPublishView&infoid=89780E928AE02608E05311C410AC3412")
--- pagetopn.py
+++ pagetopn.py
@@ -4,19 +4,24 @@ import re
 
 from collections import Counter
 from page_topn.stop_words import stop_words
+from page_topn.dynamic_page import Dynimic_process
 
-stop_worded_strs = []
-def get_page_data(url):
-    content = requests.get(url)
-    return content.text
+def get_page_data(url,flag):
+    if flag:
+        dynamic_process = Dynimic_process()
+        content = dynamic_process.start_requests(url)
+        return content
+    else:
+        content = requests.get(url)
+        return content.text
 
 def process_data(data):
     chinese = "".join(re.findall("[\u4e00-\u9fa5]+",data))
     return jieba.tokenize(chinese,mode="search")
 
 
-def page_topn(url,topn):
-    data = get_page_data(url)
+def page_topn(url, topn, flag=False):
+    data = get_page_data(url,flag)
 
     words = []
     chinese = process_data(data)
@@ -28,5 +33,5 @@ def page_topn(url,topn):
     return Counter(words).most_common(topn)
 
 if __name__ == "__main__":
-    words = page_topn("http://zfcg.wlmq.gov.cn/infopublish.do?method=infoPublishView&infoid=89780E928AE02608E05311C410AC3412",10)
+    words = page_topn("http://zfcg.wlmq.gov.cn/infopublish.do?method=infoPublishView&infoid=89780E928AE02608E05311C410AC3412",10,True)
     print(words)
